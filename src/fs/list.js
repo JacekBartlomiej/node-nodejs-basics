9d7f1eb99bbf55fc81a151c6f8c7f7c48da8ed90
Jacek <j.klosinski@ibe.edu.pl>
2024-01-27 16:35:18 +0100

    filesystem-list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from "node:fs/promises";
+
+const folderPath = "./src/fs/files";
+const errorMessage = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(folderPath);
+    console.log(files);
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
