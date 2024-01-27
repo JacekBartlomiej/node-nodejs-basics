f27b86baaba7e257de89392082765b38a2e4d6e9
Jacek <j.klosinski@ibe.edu.pl>
2024-01-27 14:31:31 +0100

    filesystem-create to do comments

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,8 +1,11 @@
 import { stat, writeFile } from "node:fs";
 
+//TODO: check how to do it with relative file path
 const path = "./src/fs/files";
 const fileName = "message.txt";
 
+
+//TODO: provide async (promise) version
 const create = async () => {
   stat(`${path}/${fileName}`, (err, stats) => {
     if (stats) {
