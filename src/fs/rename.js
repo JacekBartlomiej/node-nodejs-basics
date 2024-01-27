f10e4120fccd258adf9d792b25fcec934c0c04bb
Jacek <j.klosinski@ibe.edu.pl>
2024-01-27 16:21:51 +0100

    filesystem-rename solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { stat, rename as fsRename } from "node:fs/promises";
+
+const oldPath = "./src/fs/files/wrongFilename.txt";
+const newPath = "./src/fs/files/properFilename.md";
+const errorMessage = "FS operation failed";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await stat(newPath);
+    throw new Error(errorMessage);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      try {
+        await fsRename(oldPath, newPath);
+      } catch(err) {
+        throw new Error(errorMessage);
+      }
+    } else {
+      throw new Error(errorMessage);
+    }
+  }
 };
 
 await rename();
