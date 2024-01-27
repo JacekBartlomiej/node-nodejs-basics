6adf492164965fd939ff78823ec6c71f6ec61719
Jacek <j.klosinski@ibe.edu.pl>
2024-01-27 14:25:39 +0100

    file system - create.js task callback

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { stat, writeFile } from "node:fs";
+
+const path = "./src/fs/files";
+const fileName = "message.txt";
+
 const create = async () => {
-    // Write your code here 
+  stat(`${path}/${fileName}`, (err, stats) => {
+    if (stats) {
+      throw new Error("FS operation failed");
+    } else if (err.code === "ENOENT") {
+      writeFile(
+        `${path}/${fileName}`,
+        "I am fresh and young",
+        "utf8",
+        (err, result) => {
+          if (err) {
+            throw new Error("FS operation failed");
+          } else {
+            console.log(`The file has been saved with result: ${result}!`);
+          }
+        }
+      );
+    }
+  });
 };
 
 await create();
