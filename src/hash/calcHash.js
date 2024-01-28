57cd94bcc1b005e9cc5443a75ffbebc8576acc8b
Jacek <j.klosinski@ibe.edu.pl>
2024-01-28 11:15:47 +0100

    hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "node:fs";
+const { createHash } = await import("node:crypto");
+
+const filename = "./src/hash/files/fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const input = createReadStream(filename);
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")} ${filename}`);
+    }
+  });
 };
 
 await calculateHash();
