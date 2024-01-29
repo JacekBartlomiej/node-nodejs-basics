fb137a75e8de1d1f9f0edeb0c3a039ec27699f15
Jacek <j.klosinski@ibe.edu.pl>
2024-01-29 08:50:19 +0100

    streams-transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { compose, Transform } from 'node:stream';
+
+const reversesText = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, String(chunk).split("").reverse().join(""));
+    },
+  });
+
 const transform = async () => {
-    // Write your code here 
+    compose(process.stdin, reversesText).pipe(process.stdout)
 };
 
 await transform();
