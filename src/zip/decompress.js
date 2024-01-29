dfc6fbc9e1c64cfc717f27c786783b7e4092de00
Jacek <j.klosinski@ibe.edu.pl>
2024-01-29 09:38:28 +0100

    zlib-decompress solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { promisify } from "node:util";
+
+const pipe = promisify(pipeline);
+const sourcePath = "./src/zip/files/archive.gz";
+const destinationPath = "./src/zip/files/fileToCompress.txt";
+const errorMessage = "Decompress operation failed";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const unzip = createGunzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+    await pipe(source, unzip, destination);
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await decompress();
