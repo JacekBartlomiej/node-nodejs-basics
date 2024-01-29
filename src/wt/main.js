b41cb455ac012c1f037170929ab23ffcc7e85e3c
Jacek <j.klosinski@ibe.edu.pl>
2024-01-29 16:49:59 +0100

    worker-threads solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import os from "os";
+import { Worker } from "node:worker_threads";
+import { ReadableStreamDefaultController } from "node:stream/web";
+
+const cpus = os.cpus();
+const filePath = "./src/wt/worker.js";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workersLength = cpus.length;
+  let workersCompletedLength = 0;
+  const resultArr = [];
+  cpus.forEach((cpu, i) => {
+    const worker = new Worker(filePath);
+    const data = i + 10;
+    worker.postMessage(data);
+    worker.on("message", (result) => {
+      resultArr[i] = result;
+      workersCompletedLength++;
+      if(workersLength === workersCompletedLength) {
+        console.log(resultArr);
+      }
+    });
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,18 @@
+import { parentPort } from "node:worker_threads";
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+    parentPort.on("message", (data) => {
+        try {
+            const result = nthFibonacci(data);
+            parentPort.postMessage({ status: 'resolved', data: result});
+        } catch {
+            parentPort.postMessage({ status: 'error', data: null});
+        }
+        parentPort.close();
+    })
     // This function sends result of nthFibonacci computations to main thread
 };
 
