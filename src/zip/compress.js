2eef1da9ccaf62b12fb08980c73f7cff246ad013
Jacek <j.klosinski@ibe.edu.pl>
2024-01-29 09:20:02 +0100

    zlib-compress solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { promisify } from "node:util";
+
+const pipe = promisify(pipeline);
+const sourcePath = "./src/zip/files/fileToCompress.txt";
+const destinationPath = "./src/zip/files/archive.gz";
+const errorMessage = "Compress operation failed";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = createGzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+    await pipe(source, gzip, destination);
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await compress();
